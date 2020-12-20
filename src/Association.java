cde7f24e47351fb1efb56493231ed402dec15f51
Adam Kogut <adamkgt@gmail.com>
2020-12-20 21:02:33 +0100

    #3 Zadanie2. Program, który dla podanego koloru (Pierwsza litera), odpowie z czym mu się kojarzy.

---
+++ src/Association.java
@@ -0,0 +1,24 @@
+public class Association {
+    public static void main(String[] args) {
+
+        char letter = 'k';
+
+        switch(letter){
+            case 'c':
+                System.out.println("Pomidor");
+                break;
+            case'z':
+                System.out.println("Trawa");
+                break;
+            case'n':
+                System.out.println("Morze");
+                break;
+            default:
+                System.out.println("Brak Skojarzeń");
+        }
+
+
+
+
+    }
+}
